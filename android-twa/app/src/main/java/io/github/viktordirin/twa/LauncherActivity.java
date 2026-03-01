9594f95fee428ac770698a9240bf5337f84a5901
Viktor <vdirin@gmail.com>
2026-03-01 14:06:58 +0700

    build: fix TWA assembly, signing and assetlinks synchronization
    * Configure Java 17 via org.gradle.java.home for Gradle compatibility
    * Generate new android.keystore and implement automated signing
    * Sync applicationId to io.github.viktordirin.twa in build.gradle
    * Update assetlinks.json with new SHA-256 fingerprint from keystore
    * Add _config.yml to ensure Jekyll serves .well-known directory
    * Update LauncherActivity for transparent status bar and exit on back
    * Include 'Clear App Cache' feature in settings

--- android-twa/app/src/main/java/io/github/viktordirin/twa/LauncherActivity.java
+++ android-twa/app/src/main/java/io/github/viktordirin/twa/LauncherActivity.java
@@ -1,54 +1,42 @@
-/*
- * Copyright 2020 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.github.viktordirin.twa;
 
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.view.WindowManager;
 
 public class LauncherActivity
         extends com.google.androidbrowserhelper.trusted.LauncherActivity {
-    
-
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
+
+        // Установка портретной ориентации для Android 8+
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
+
+        // Делаем статус-бар прозрачным и контент "под" ним, чтобы убрать черную полосу
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
         }
     }
 
     @Override
+    public void onBackPressed() {
+        // Принудительное закрытие приложения при нажатии "Назад"
+        // Это решит проблему с залипшим окном подтверждения
+        finish();
+    }
+
+    @Override
     protected Uri getLaunchingUrl() {
-        // Get the original launch Url.
-        Uri uri = super.getLaunchingUrl();
-
-        
-
-        return uri;
+        return super.getLaunchingUrl();
     }
 }
